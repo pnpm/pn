2958cf4ee2fdbcaf18ba659fff822c5d3f5d53b2
Zoltan Kochan <z@kochan.io>
2023-06-06 03:10:12 +0300

    style: fix

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,6 +1,6 @@
-use std::process::Command;
-use std::fs;
 use assert_cmd::prelude::*;
+use std::fs;
+use std::process::Command;
 
 #[test]
 fn test_run_script() {
@@ -8,19 +8,22 @@ fn test_run_script() {
     // Create a temporary directory and a package.json file
     let temp_dir = tempfile::tempdir().unwrap();
     let package_json_path = temp_dir.path().join("package.json");
-    fs::write(&package_json_path, r#"{"scripts": {"test": "echo hello world"}}"#).unwrap();
+    fs::write(
+        &package_json_path,
+        r#"{"scripts": {"test": "echo hello world"}}"#,
+    )
+    .unwrap();
 
     // When
     // Run the CLI with the "run" command
     let mut cmd = Command::cargo_bin("pn").unwrap();
     cmd.current_dir(temp_dir.path())
-       .arg("run")
-       .arg("test")
-       .assert()
-       .success()
-       .stdout("hello world\n");
+        .arg("run")
+        .arg("test")
+        .assert()
+        .success()
+        .stdout("hello world\n");
 
     // Clean up
     temp_dir.close().unwrap();
 }
-

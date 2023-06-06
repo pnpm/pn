4976d82543110d24b05421dc54eb8ee818d454c1
Zoltan Kochan <z@kochan.io>
2023-06-06 03:05:49 +0300

    test: running a script

---
+++ tests/test_main.rs
@@ -0,0 +1,26 @@
+use std::process::Command;
+use std::fs;
+use assert_cmd::prelude::*;
+
+#[test]
+fn test_run_script() {
+    // Given
+    // Create a temporary directory and a package.json file
+    let temp_dir = tempfile::tempdir().unwrap();
+    let package_json_path = temp_dir.path().join("package.json");
+    fs::write(&package_json_path, r#"{"scripts": {"test": "echo hello world"}}"#).unwrap();
+
+    // When
+    // Run the CLI with the "run" command
+    let mut cmd = Command::cargo_bin("pn").unwrap();
+    cmd.current_dir(temp_dir.path())
+       .arg("run")
+       .arg("test")
+       .assert()
+       .success()
+       .stdout("hello world\n");
+
+    // Clean up
+    temp_dir.close().unwrap();
+}
+

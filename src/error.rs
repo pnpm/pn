a3dbd3b44b9e4258b6652dfc7973ffc3ff33a713
khai96_ <hvksmr1996@gmail.com>
2023-06-14 20:55:45 +0700

    Add spaces between groups of lines

--- src/error.rs
+++ src/error.rs
@@ -7,18 +7,23 @@ pub enum PnError {
     /// Script not found when running `pn run`.
     #[display(fmt = "Missing script: {name}")]
     MissingScript { name: String },
+
     /// Script ran by `pn run` exits with non-zero status code.
     #[display(fmt = "Command failed with exit code {status}")]
     ScriptError { name: String, status: NonZeroI32 },
+
     /// Subprocess finishes but without a status code.
     #[display(fmt = "Command {command:?} has ended unexpectedly")]
     UnexpectedTermination { command: String },
+
     /// The program receives --workspace-root outside a workspace.
     #[display(fmt = "--workspace-root may only be used in a workspace")]
     NotInWorkspace,
+
     /// Failed to prepend `node_modules/.bin` to `PATH`.
     #[display(fmt = "Cannot add `node_modules/.bin` to PATH: {error}")]
     NodeBinPathError { error: JoinPathsError },
+
     /// Other errors.
     #[display(fmt = "{error}")]
     Other { error: Box<dyn Error> },
@@ -29,6 +34,7 @@ pub enum PnError {
 pub enum MainError {
     /// Errors emitted by `pn` itself.
     Pn(PnError),
+
     /// The subprocess that takes control exits with non-zero status code.
     Sub(NonZeroI32),
 }

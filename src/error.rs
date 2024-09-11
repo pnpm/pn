2b7628cc036a604aedc0f1edd98cc5178ee80cac
John Vandenberg <jayvdb@gmail.com>
2024-09-11 20:39:06 +0800

    Update dependencies (#61)
    Includes changing the Rust version from 1.70 to 1.80

--- src/error.rs
+++ src/error.rs
@@ -6,39 +6,39 @@ use std::{env::JoinPathsError, io, num::NonZeroI32, path::PathBuf};
 #[derive(Debug, Display)]
 pub enum PnError {
     /// Script not found when running `pn run`.
-    #[display(fmt = "Missing script: {name}")]
+    #[display("Missing script: {name}")]
     MissingScript { name: String },
 
     /// Script ran by `pn run` exits with non-zero status code.
-    #[display(fmt = "Command failed with exit code {status}")]
+    #[display("Command failed with exit code {status}")]
     ScriptError { name: String, status: NonZeroI32 },
 
     /// Subprocess finishes but without a status code.
-    #[display(fmt = "Command ended unexpectedly: {command}")]
+    #[display("Command ended unexpectedly: {command}")]
     UnexpectedTermination { command: ShellQuoted },
 
     /// Fail to spawn a subprocess.
-    #[display(fmt = "Failed to spawn process: {_0}")]
+    #[display("Failed to spawn process: {_0}")]
     SpawnProcessError(io::Error),
 
     /// Fail to wait for the subprocess to finish.
-    #[display(fmt = "Failed to wait for the process: {_0}")]
+    #[display("Failed to wait for the process: {_0}")]
     WaitProcessError(io::Error),
 
     /// The program receives --workspace-root outside a workspace.
-    #[display(fmt = "--workspace-root may only be used in a workspace")]
+    #[display("--workspace-root may only be used in a workspace")]
     NotInWorkspace,
 
     /// No package manifest.
-    #[display(fmt = "File not found: {file:?}")]
+    #[display("File not found: {file:?}")]
     NoPkgManifest { file: PathBuf },
 
     /// Error related to filesystem operation.
-    #[display(fmt = "{path:?}: {error}")]
+    #[display("{path:?}: {error}")]
     FsError { path: PathBuf, error: io::Error },
 
     /// Error emitted by [`lets_find_up`]'s functions.
-    #[display(fmt = "Failed to find {file_name:?} from {start_dir:?} upward: {error}")]
+    #[display("Failed to find {file_name:?} from {start_dir:?} upward: {error}")]
     FindUpError {
         start_dir: PathBuf,
         file_name: &'static str,
@@ -46,15 +46,15 @@ pub enum PnError {
     },
 
     /// An error is encountered when write to stdout.
-    #[display(fmt = "Failed to write to stdout: {_0}")]
+    #[display("Failed to write to stdout: {_0}")]
     WriteStdoutError(io::Error),
 
     /// Parse JSON error.
-    #[display(fmt = "Failed to parse {file:?}: {message}")]
+    #[display("Failed to parse {file:?}: {message}")]
     ParseJsonError { file: PathBuf, message: String },
 
     /// Failed to prepend `node_modules/.bin` to `PATH`.
-    #[display(fmt = "Cannot add `node_modules/.bin` to PATH: {_0}")]
+    #[display("Cannot add `node_modules/.bin` to PATH: {_0}")]
     NodeBinPathError(JoinPathsError),
 }
 
